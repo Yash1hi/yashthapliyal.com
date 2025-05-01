304910323576f5efd3cf59d602a2b3274db5bdfe
Yash Thapliyal <yash.thapliyal.007@gmail.com>
2025-05-01 12:53:15 -0600

    centered squeeble

--- src/pages/P5Sketch.tsx
+++ src/pages/P5Sketch.tsx
@@ -9,11 +9,13 @@ const P5Sketch = () => {
       <div className="container mx-auto px-4 py-20">
         {/* <h1 className="text-3xl font-bold mb-6">Interactive Sketch</h1> */}
         <div className="w-full aspect-video">
-          <iframe 
-            src="https://editor.p5js.org/yash.thapliyal.007/full/Ko7twdgmG"
-            className="w-full h-full border-2 border-black rounded-lg"
-            title="p5.js Sketch"
-          ></iframe>
+          <div className="flex justify-center items-center">
+            <iframe 
+              src="https://editor.p5js.org/yash.thapliyal.007/full/Ko7twdgmG"
+              className="w-[500px] h-[500px] border-2 border-black rounded-lg"
+              title="p5.js Sketch"
+            ></iframe>
+          </div>
         </div>
       </div>
       <Footer />
